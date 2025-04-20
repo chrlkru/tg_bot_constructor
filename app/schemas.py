e95ba707c8ee32314e8ab9886b655306df666f95
Дмитрий Филимонов <filimonovdima324@gmail.com>
2025-04-20 17:02:40 +0300

    Структурировал проект: добавил backend/ и frontend/ папки

--- app/schemas.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import Dict
-
-class BotProject(BaseModel):
-    name: str
-    template_type: str
-    description: str
-    token: str   # <-- Добавили поле token
-    content: Dict
